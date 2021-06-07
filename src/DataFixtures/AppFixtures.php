66e20ea9ff3a0673e418bf949dd1a1473ca1bde0
zackdev <zacchariedevoulx@gmail.com>
2021-06-07 13:48:24 +0200

    closes #9; Add some faked users into the database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,16 +3,36 @@
 namespace App\DataFixtures;
 
 use App\Entity\Posts;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    private function encode($user, $plaintextpassword)
+    {
+        return $this->passwordEncoder->encodePassword(
+            $user,
+            $plaintextpassword
+        );
+    }
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create("fr_FR");
         for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setNickname($faker->firstName());
+            $user->setPassword($this->encode($user, "toto"));
+            $user->setRoles(["USER_ROLE"]);
+            $manager->persist($user);
+
             $onePost = new Posts;
             $onePost->setCreatedAt($faker->dateTime($max = "now", null));
             $onePost->setTitle($faker->sentence(5));
